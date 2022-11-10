e98ac87041ccd8e8a4db83285cf53bb63caa686f
Christopher Medykiewicz <Christopher.Medykiewicz@newrez.com>
2022-11-09 22:22:20 -0500

    adding new auth pages

--- apps/cruxi/src/app/login/login.tsx
+++ apps/cruxi/src/app/login/login.tsx
@@ -1,18 +1,58 @@
-import styled from 'styled-components';
+import React, { useRef, useState } from "react"
+import { Form, Button, Card, Alert } from "react-bootstrap"
+import { useAuth } from "../contexts/AuthContext"
+import { Link, useHistory } from "react-router-dom"
 
-/* eslint-disable-next-line */
-export interface LoginProps {}
+export default function Login() {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const { login } = useAuth()
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+  const history = useHistory()
 
-const StyledLogin = styled.div`
-  color: pink;
-`;
+  async function handleSubmit(e) {
+    e.preventDefault()
 
-export function Login(props: LoginProps) {
+    try {
+      setError("")
+      setLoading(true)
+      await login(emailRef.current.value, passwordRef.current.value)
+      history.push("/")
+    } catch {
+      setError("Failed to log in")
+    }
+
+    setLoading(false)
+  }
+
   return (
-    <StyledLogin>
-      <h1>Welcome to Login!</h1>
-    </StyledLogin>
-  );
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Log In</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
+              Log In
+            </Button>
+          </Form>
+          <div className="w-100 text-center mt-3">
+            <Link to="/forgot-password">Forgot Password?</Link>
+          </div>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Need an account? <Link to="/signup">Sign Up</Link>
+      </div>
+    </>
+  )
 }
-
-export default Login;
